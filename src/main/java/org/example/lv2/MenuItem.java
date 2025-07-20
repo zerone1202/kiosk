05b97ba25a944d3624bc6fb683bfed0cd42bc347
zerone <bazz0324@naver.com>
2025-07-21 03:13:00 +0900

    price 자료형 수정: double → Double

--- src/main/java/org/example/lv2/MenuItem.java
+++ src/main/java/org/example/lv2/MenuItem.java
@@ -10,7 +10,7 @@ public class MenuItem {
     private String description; // '설명'
 
     // 생성자
-    public MenuItem(String name, double price, String description) {
+    public MenuItem(String name, Double price, String description) {
         this.name = name;
         this.price = price;
         this.description = description;
